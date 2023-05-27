f9ad29a8f4eb7df600fd6ab5054f6a20124ed5f0
alexbaumgertner <alex.baumgertner@gmail.com>
2023-05-27 14:10:35 +0300

    Fix reset page component name

--- pages/reset-password/index.tsx
+++ pages/reset-password/index.tsx
@@ -5,7 +5,7 @@ import css from './reset-password.module.css'
 import { useEffect, useState } from 'react'
 import { reset } from '../frontend-api/auth'
 
-const RemindPasswordPage: NextPage = () => {
+const ResetPasswordPage: NextPage = () => {
   const [state, setState] = useState<'loading' | 'error' | 'default'>('default')
   const [resetData, setResetData] = useState<{ email: string }>({ email: '' })
 
@@ -30,4 +30,4 @@ const RemindPasswordPage: NextPage = () => {
   )
 }
 
-export default RemindPasswordPage
+export default ResetPasswordPage
