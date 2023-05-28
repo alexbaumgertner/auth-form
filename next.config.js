82b63d9ffac0756d7e064341442735e0eb5ec193
alexbaumgertner <alex.baumgertner@gmail.com>
2023-05-28 21:55:13 +0300

    Don't deploy to GH pages

--- next.config.js
+++ next.config.js
@@ -1,29 +1,7 @@
-const IS_PROD = process.env.NODE_ENV === 'production'
-const PREFIX = IS_PROD ? '/auth-form' : ''
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: IS_PROD ? PREFIX : undefined,
-  async redirects () {
-    return IS_PROD ? [
-      {
-        source: '/reset-password',
-        destination: `/${PREFIX}/reset-password`,
-        permanent: true,
-      },
-      {
-        source: '/create-account',
-        destination: `/${PREFIX}/create-account`,
-        permanent: true,
-      },
-      {
-        source: '/login',
-        destination: `/${PREFIX}/login`,
-        permanent: true,
-      },
-    ] : []
-  },
 }
 
 module.exports = nextConfig
